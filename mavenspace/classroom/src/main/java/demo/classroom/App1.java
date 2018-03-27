6e7440174d0556afa99c868c6fd3ece4f129da78
Deepu9992 <37417452+Deepu9992@users.noreply.github.com>
2018-03-27 08:51:41 -0400

    Add files via upload

---
+++ mavenspace/classroom/src/main/java/demo/classroom/App1.java
@@ -0,0 +1,10 @@
+package demo.classroom;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hey Deep Welcome to test Project");
+	}
+
+}
